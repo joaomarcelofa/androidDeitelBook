89b93426270f1abfb04a87b70e2ebcf01268541d
joaomarcelofa <joaov7.1996@gmail.com>
2017-06-24 22:27:31 -0300

    Estrutura de layout completa

---
+++ FavoriteTwitterSeacrhes/app/src/main/java/joao/com/favoritetwitterseacrhes/FavoriteTwitterSearches.java
@@ -0,0 +1,13 @@
+package joao.com.favoritetwitterseacrhes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteTwitterSearches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_twitter_searches);
+    }
+}
