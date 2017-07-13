532e4c6327462268d6d56381a19517ffba2401ed
João Marcelo Ferreira Arrabal <JMFArrabal@STI_0013883.cremesp.org.br>
2017-07-13 19:46:42 -0300

    Adicionado layouts XML e variáveis de intância da classe main

--- FlagQuizGame/app/src/main/java/com/example/jmfarrabal/flagquizgame/main.java
+++ FlagQuizGame/app/src/main/java/com/example/jmfarrabal/flagquizgame/main.java
@@ -3,7 +3,34 @@ package com.example.jmfarrabal.flagquizgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TableLayout;
+
 public class main extends AppCompatActivity {
+
+    //Variáveis de instânica
+
+    private List<String> fileNameList; //nomes dos aquivos das bandeiras
+    private List<String> quizCountriestList; // Nomes dos países do teste
+    private Map<String, Boolean> regionsMap; // Quais regiões estão habilitadas
+    private String correctAnswer; // País correto para a bandeira corrente
+    private int totalGuesses; // Número de palpites dados
+    private int correctAnswers; //Número de palpites certos
+    private int guessRows; // Número de linhas que exibem escolhas
+    private Random random; // gerador de números aleatórios
+    private Handler handler; // Usado para atrasar o carregamento da próxima bandeira
+    private Animation shakeAnimation; // Animação para palpite incorreto
+
+    private TextView answerTextView;
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private TableLayout buttonTableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
